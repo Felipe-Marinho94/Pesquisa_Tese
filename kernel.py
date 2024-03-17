5775701649d11286d67416cac8fc7c506ee50b1e
Felipe Pinto Marinho <59834239+Felipe-Marinho94@users.noreply.github.com>
2024-03-17 20:47:49 -0300

    Add files via upload

--- kernel.py
+++ kernel.py
@@ -8,12 +8,13 @@ Data:13/04/2024
 #Carregando alguns pacotes relevantes
 #------------------------------------------------------------------------------
 import numpy as np
-import sklearn
+from sklearn.metrics import accuracy_score, r2_score, mean_squared_error
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from numpy import linalg
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, make_regression
 from matplotlib import style
-from Gradiente conjugado import CG
+
 
 #------------------------------------------------------------------------------
 #Implementação de algumas funções de kernel
@@ -35,21 +36,22 @@ def gaussiano_kernel(x, y, gamma = 0.5):
     return np.exp(-gamma * linalg.norm(x - y)**2)
 
 #------------------------------------------------------------------------------
-#Implementação do método fit() utilizando o CG de Hestenes-Stiefel
+#Implementação do método fit() para problemas de classificação e regressão
+#utilizando o CG de Hestenes-Stiefel
 #fontes: 
 #    -SUYKENS, Johan AK; VANDEWALLE, Joos. Least squares support vector machine classifiers.
 #     Neural processing letters, v. 9, p. 293-300, 1999.
 #
 #    -GOLUB, Gene H.; VAN LOAN, Charles F. Matrix computations. JHU press, 2013.   
 #------------------------------------------------------------------------------
-def fit(X, y, tau, kernel):
+def fit_class(X, y, tau, kernel):
     #Inputs
     #X: array das variáveis de entrada array(n x p)
     #y: array de rótulos (classificação), variável de saída (regressão) array (n x 1)
     #tau: termo de regularização do problema primal do LSSVM (escalar)
     #kernel: string indicando o kernel utilizado ("linear", "polinomial", "gaussiano")
     
-    n_samples, n_feautres = X.shape
+    n_samples, n_features = X.shape
     
     #Matriz de Gram
     K = np.zeros((n_samples, n_samples))
@@ -85,9 +87,9 @@ def fit(X, y, tau, kernel):
     
     #Construção do vetor B
     d1 = 0
-    d2 = np.expand_dims(np.ones(100), axis = 1)
-    b1 = np.expand_dims(np.array(np.dot(y, np.linalg.inv(H).dot(y))), axis = 0)
-    B = np.concatenate((np.expand_dims(b1, axis = 1), d2), axis = 0)
+    d2 = np.expand_dims(np.ones(n_samples), axis = 1)
+    b1 = np.expand_dims(np.array(np.dot(y, np.linalg.inv(H).dot(d2))), axis = 0)
+    B = np.concatenate((b1, d2), axis = 0)
     B = np.squeeze(B)
     
     #Aplicação de um método iterativo para a solução do sistema Ax = B
@@ -102,44 +104,176 @@ def fit(X, y, tau, kernel):
                  "kernel": K}
     
     return resultado
-    
 
-def predict_class(alphas, b, K, X):
-    estimado = []
-    for i in range(X.shape[0]):
-        
+
+def fit_regre(X, y, tau, kernel):
+    #Inputs
+    #X: array das variáveis de entrada array(n x p)
+    #y: array de rótulos (classificação), variável de saída (regressão) array (n x 1)
+    #tau: termo de regularização do problema primal do LSSVM (escalar)
+    #kernel: string indicando o kernel utilizado ("linear", "polinomial", "gaussiano")
     
-    return np.sign
+    n_samples, n_features = X.shape
+    
+    #Matriz de Gram
+    K = np.zeros((n_samples, n_samples))
+    for i in range(n_samples):
+        for j in range(n_samples):
+            
+            #Kernel trick
+            if kernel == "linear":
+                K[i, j] = linear_kernel(X[i], X[j])
+            
+            if kernel == "gaussiano":
+                K[i, j] = gaussiano_kernel(X[i], X[j])
+            
+            if kernel == "polinomial":
+                K[i, j] = polinomial_kernel(X[i], X[j])
+    
+    #Construção da Matriz Omega
+    Omega = K
+    
+    #--------------------------------------------------------------------------
+    #Construção do sistema linear com matriz dos coeficientes
+    #simétrica, definda positiva: Ax = B
+    #--------------------------------------------------------------------------
+    #Construção da matriz A
+    H = Omega + (1/tau) * np.identity(n_samples)
+    um_coluna = np.ones((n_samples))
+    s = np.dot(um_coluna, np.linalg.inv(H).dot(um_coluna))
+    zero_linha = np.zeros((1, n_samples))
+    zero_coluna = np.zeros((n_samples, 1))
+    A = np.block([[s, zero_linha], [zero_coluna, H]])
+    
+    #Construção do vetor B
+    d1 = 0
+    d2 = np.expand_dims(y, axis = 1)
+    b1 = np.expand_dims(np.array(np.dot(um_coluna, np.linalg.inv(H).dot(y))), axis = 0)
+    B = np.concatenate((np.expand_dims(b1, axis = 1), d2), axis = 0)
+    B = np.squeeze(B)
+    
+    #Aplicação de um método iterativo para a solução do sistema Ax = B
+    x_inicial = np.zeros(n_samples + 1)
+    solution = CG_conditioned(A, x_inicial ,B)
+    
+    #Obtenção do b e dos multiplicadores de Lagrange
+    b = solution[0]
+    alphas = solution[1:] - np.linalg.inv(H).dot(um_coluna) * b
+    
+    resultado = {'b': b,
+                 "mult_lagrange": alphas,
+                 "kernel": K}
+    
+    return resultado
+
+
+    
+#------------------------------------------------------------------------------
+#Implementação do método predict() para problemas de classificação e regressão
+#utilizando o CG de Hestenes-Stiefel
+#fontes: 
+#    -SUYKENS, Johan AK; VANDEWALLE, Joos. Least squares support vector machine classifiers.
+#     Neural processing letters, v. 9, p. 293-300, 1999.
+#
+#    -GOLUB, Gene H.; VAN LOAN, Charles F. Matrix computations. JHU press, 2013.   
+#------------------------------------------------------------------------------
+def predict_class(alphas, b, kernel, X_treino, y_treino, X_teste):
+    #Inicialização
+    estimado = np.zeros(X_teste.shape[0])
+    n_samples_treino = X_treino.shape[0]
+    n_samples_teste = X_teste.shape[0]
+    K = np.zeros((n_samples_teste, n_samples_treino))
+    
+    #Construção da matriz de Kernel
+    for i in range(n_samples_teste):
+        for j in range(n_samples_treino):
+            
+            if kernel == "linear":
+                K[i, j] = linear_kernel(X_teste[i], X_treino[j])
+            
+            if kernel == "polinomial":
+                K[i, j] = polinomial_kernel(X_teste[i], X_treino[j])
+            
+            if kernel == "gaussiano":
+                K[i, j] = gaussiano_kernel(X_teste[i], X_treino[j])
+            
+        #Realização da predição
+        estimado[i] = np.sign(np.sum(np.multiply(np.multiply(alphas, y_treino), K[i])) + b)
+    
+    return estimado
+
+def predict_regre(alphas, b, kernel, X_treino, X_teste):
+    #Inicialização
+    estimado = np.zeros(X_teste.shape[0])
+    n_samples_treino = X_treino.shape[0]
+    n_samples_teste = X_teste.shape[0]
+    K = np.zeros((n_samples_teste, n_samples_treino))
+    
+    #Construção da matriz de Kernel
+    for i in range(n_samples_teste):
+        for j in range(n_samples_treino):
+            
+            if kernel == "linear":
+                K[i, j] = linear_kernel(X_teste[i], X_treino[j])
+            
+            if kernel == "polinomial":
+                K[i, j] = polinomial_kernel(X_teste[i], X_treino[j])
+            
+            if kernel == "gaussiano":
+                K[i, j] = gaussiano_kernel(X_teste[i], X_treino[j])
+            
+        #Realização da predição
+        estimado[i] = np.sum(np.multiply(alphas, K[i])) + b
+    
+    return estimado
 
 
 #------------------------------------------------------------------------------
 #Realizando um pequeno teste
-#Dataset sintético
+#Dataset sintético classificação
 #------------------------------------------------------------------------------
 style.use("fivethirtyeight")
  
-X, y = make_blobs(n_samples = 100, centers = 3, 
+X, y = make_blobs(n_samples = 300, centers = 2, 
                cluster_std = 1, n_features = 2)
  
+
+
 plt.scatter(X[:, 0], X[:, 1], s = 40, color = 'g')
 plt.xlabel("X")
 plt.ylabel("Y")
- 
+X[1]
 plt.show()
 plt.clf()
 
-resultado = fit(X, y, 0.5, "gaussiano")
-H = O + (1/100) * np.identity(100)
+for i in range(len(y)):
+    if y[i] == 0:
+        y[i] = -1
 
-s = np.array(np.dot(y, np.linalg.inv(H).dot(y)))
-s = np.expand_dims(s, axis = 0)
-zero_linha = np.zeros((1, 100))
-zero_coluna = np.zeros((100, 1))
-A = np.block([[s, zero_linha], [zero_coluna, H]])
-d2 = np.expand_dims(np.ones(100), axis = 1)
-b1 = np.expand_dims(np.array(np.dot(y, np.linalg.inv(H).dot(y))), axis = 0)
-B = np.concatenate((np.expand_dims(b1, axis = 1), d2), axis = 0)
-B = np.squeeze(B)
-solution = CG(A, B, 0.1)
-b = solution[0]
-alphas = solution[1:] - np.linalg.inv(H).dot(y) * b
+#Divisão treino/teste
+X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, test_size = 0.3,
+                                                        random_state= 49)
+
+resultado = fit_class(X_treino, y_treino, 0.5, "gaussiano")
+alphas = resultado['mult_lagrange']
+b = resultado['b']
+lssvm_hat = predict_class(alphas, b, "gaussiano", X_treino, y_treino, X_teste)
+accuracy_score(y_teste, lssvm_hat)
+
+#------------------------------------------------------------------------------
+#Realizando um pequeno teste
+#Dataset sintético regressão
+#------------------------------------------------------------------------------
+X, y = make_regression(n_samples=300, n_features=2, noise=1, random_state=42)
+
+#Divisão treino/teste
+X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, test_size = 0.3,
+                                                        random_state=42)
+
+
+resultado = fit_regre(X_treino, y_treino, 0.5, "gaussiano")
+alphas = resultado['mult_lagrange']
+b = resultado['b']
+lssvm_hat = predict_regre(alphas, b, "gaussiano", X_treino, X_teste)
+r2_score(y_teste, lssvm_hat)
+np.sqrt(mean_squared_error(y_teste, lssvm_hat))
