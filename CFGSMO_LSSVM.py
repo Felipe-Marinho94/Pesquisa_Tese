917a12635c9bce2fbb71dc685227b4caf59c7a10
Felipe Pinto Marinho <59834239+Felipe-Marinho94@users.noreply.github.com>
2024-08-13 23:53:12 -0300

    Add files via upload

--- CFGSMO_LSSVM.py
+++ CFGSMO_LSSVM.py
@@ -14,10 +14,45 @@ import seaborn as sns
 from kernel import gaussiano_kernel, linear_kernel, polinomial_kernel
 
 #---------------------------------------------------------------
+#Implementação de alguma funções relevantes
+#---------------------------------------------------------------
+def FGWSS(gradient, K):
+    '''
+    Interface do Método
+    Ação: Visa selecionar os índices dos dois multiplicadores de
+    Lagrange que serão atualizados, por meio da heurística do
+    método Functional Gain Working Selection Stategy (FGWSS)
+
+    INPUT:
+    gradient: Vetor Gradiente (array n)
+    K: Matriz de Kernel (array n x n)
+
+    OUTPUT:
+    os dois índices selecionados
+    '''
+    
+    #Primeiro índice
+    i = np.argmax(np.absolute(gradient))
+
+    #Segundo índice
+    exception = i
+    m = np.zeros(gradient.shape[0], dtype=bool)
+    m[exception] = True
+
+    numerador = (gradient-gradient[i])**2
+    denominador = 2 * (K[i, i] * np.ones(K.shape[0]) + np.diag(K) - K[i, :] - K[:, i])
+    quociente = np.ma.array(numerador/denominador, mask=m)
+    j = np.argmax(quociente)
+
+    return (i, j)
+
+
+
+#---------------------------------------------------------------
 #Função para ajustar o modelo LSSVM utilizando o método CFGSMO
 #SOURCE: https://link.springer.com/article/10.1007/s00521-022-07875-1
 #---------------------------------------------------------------
-def fit_CFGSMO_LSSVM(X, y, gamma, kernel, epsilon):
+def fit_CFGSMO_LSSVM(X, y, gamma, kernel, epsilon, N):
     '''
     Interface do método
     Ação: Este método visa realizar o ajuste do modelo LSSVM empregando
@@ -30,23 +65,11 @@ def fit_CFGSMO_LSSVM(X, y, gamma, kernel, epsilon):
     gamma: termo de regularização de Tichonov (escalar)
     kernel: função de kernel utilizada (string: "linear", "polinomial", "gaussiano");
     epsilon: Tolerância (Critério de parada)
+    N: Número máximo de iterações
 
     OUTPUT:
     vetor ótimo de multiplicadores de Lagrange estimados.
     '''
-    #Inicialização
-    #Multiplicadores de Lagrange
-    alphas = np.zeros(X.shape[0], 1)
-
-    #Gradiente
-    Gradient = np.zeros(X.shape[0], 1)
-
-    #direções conjugadas
-    s = np.zeros(X.shape[0], 1)
-    t = np.zeros(X.shape[0], 1)
-
-    #Termo tau
-    tau = 1
 
     #Construindo a matriz de kernel
     n_samples, n_features = X.shape
@@ -70,3 +93,135 @@ def fit_CFGSMO_LSSVM(X, y, gamma, kernel, epsilon):
     #Regularização de Tichonov
     K_tilde = K + (1/gamma)*np.diag(np.full(K.shape[0], 1))
 
+    #Inicialização
+    #Multiplicadores de Lagrange
+    alphas = np.zeros(n_samples)
+
+    #Gradiente
+    Gradient = np.random.normal(loc = 0, scale=1, size=n_samples)
+
+    #direções conjugadas
+    s = np.zeros(n_samples)
+    t = np.zeros(n_samples)
+
+    #Termo tau
+    tau = 1
+
+    #Controle de iteração
+    k = 0
+
+    #erro
+    erro = []
+
+    #Laço de iteração
+    while k <= N:
+
+        #incremento
+        k = k + 1
+
+        #Seleção do par de multiplicadores de Lagrange para atualização
+        i, j = FGWSS(Gradient, K_tilde)
+
+        #Realizando as atualizações
+        r = (t[i] - t[j])/tau
+        s = np.eye(1, n_samples, i)[0] - np.eye(1, n_samples, j)[0] + r * s
+        t = K_tilde[:, j] - K_tilde[:, i] + r * t
+        tau = t[j] - t[i]
+
+        #Calculando o parâmetro rho
+        rho = (Gradient[i] - Gradient[j])/tau
+
+        #Atualização da variável Dual
+        alphas = alphas + rho * s
+
+        #Atualização do gradiente
+        Gradient = Gradient + rho * t
+
+        #Armazenando o erro
+        erro.append(np.argmax(Gradient) - np.argmin(Gradient))
+
+        #Condição de parada
+        if np.abs(np.argmax(Gradient) - np.argmin(Gradient)) <= epsilon:
+            break
+    
+    #Resultados
+    resultados = {"mult_lagrange": alphas,
+                  "erro": erro}
+    
+    return resultados
+    
+
+        
+#Realizando alguns testes
+K = np.random.normal(loc=0, scale=1, size=(100, 100))
+A = 2 * (K[1, 1] * np.ones(K.shape[0]) + np.diag(K) - K[1, :] - K[:, 1])
+G = np.random.normal(loc=0, scale=1, size=(100))
+np.argmax(G/A)
+
+X = np.random.normal(10, 1 , size=(100, 5))
+y = np.random.normal(10, 1, 100)
+
+resultados = fit_CFGSMO_LSSVM(X, y, 100, 'gaussiano', 0.01, 80)
+len(resultados['mult_lagrange'])
+
+#Regularização de Tichonov
+K_tilde = K + (1/1000)*np.diag(np.full(K.shape[0], 1))
+
+#Inicialização
+#Multiplicadores de Lagrange
+alphas = np.zeros(100)
+
+#Gradiente
+Gradient = np.random.normal(loc = 0, scale=1, size=100)
+
+#direções conjugadas
+s = np.zeros(100)
+t = np.zeros(100)
+
+#Termo tau
+tau = 1
+
+#Controle de iteração
+k = 0
+
+#erro
+erro = []
+
+#Laço de iteração
+while k <= 10:
+
+        #incremento
+        k =+ 1
+
+        #Seleção do par de multiplicadores de Lagrange para atualização
+        i, j = FGWSS(Gradient, K_tilde)
+
+        #Realizando as atualizações
+        r = (t[i] - t[j])/tau
+        s = np.eye(1, 100, i) - np.eye(1, 100, j) + r * s
+        t = K_tilde[:, j] - K_tilde[:, i] + r * t
+        tau = t[j] - t[i]
+        
+
+        #Calculando o parâmetro rho
+        rho = (Gradient[i] - Gradient[j])/tau
+        
+        #Atualização da variável Dual
+        alphas =+ rho * s
+        
+
+        #Atualização do gradiente
+        Gradient =+  rho * t
+        
+        #Armazenando o erro
+        erro.append(np.argmax(Gradient) - np.argmin(Gradient))
+
+        #Condição de parada
+        if (np.argmax(Gradient) - np.argmin(Gradient)) < 0.01:
+            break
+
+Gradient
+i
+j
+tau
+np.eye(1, 100, 3)[0]
